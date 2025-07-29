8f842de9068e0c0dc318ae02d75e6d85674e5e95
zaibeihou <2750164330@qq.com>
2025-07-29 16:57:13 +0800

    test1

--- kernel/defs.h
+++ kernel/defs.h
@@ -67,6 +67,8 @@ void            ramdiskrw(struct buf*);
 void*           kalloc(void);
 void            kfree(void *);
 void            kinit(void);
+void *          ksteal(int);
+ 
 
 // log.c
 void            initlog(int, struct superblock*);
--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -21,12 +21,16 @@ struct run {
 struct {
   struct spinlock lock;
   struct run *freelist;
-} kmem;
+} kmem[NCPU];
 
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
+  for(int i = 0; i < NCPU; i++){
+    char name[9] = {0};
+    snprintf(name, 8, "kmem-%d", i);
+    initlock(&kmem[i].lock, name);
+  }
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -51,15 +55,18 @@ kfree(void *pa)
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
 
+  push_off();
+  int cpu = cpuid();
   // Fill with junk to catch dangling refs.
   memset(pa, 1, PGSIZE);
-
   r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  acquire(&kmem[cpu].lock);
+  r->next = kmem[cpu].freelist;
+  kmem[cpu].freelist = r;
+  release(&kmem[cpu].lock);
+
+  pop_off();
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -69,14 +76,37 @@ void *
 kalloc(void)
 {
   struct run *r;
-
-  acquire(&kmem.lock);
-  r = kmem.freelist;
+  push_off();
+  int cpu = cpuid();
+  acquire(&kmem[cpu].lock);
+  r = kmem[cpu].freelist;
   if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+    kmem[cpu].freelist = r->next;
+  release(&kmem[cpu].lock);
+  if(r == 0){
+    r = ksteal(cpu);
+  }
 
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
+  pop_off();
+  return (void*)r;
+}
+
+ void *
+ ksteal(int cpu){
+  struct run *r;
+  for(int i = 0; i < NCPU; i++){
+    int cpu_next = (cpu + i) % NCPU;
+    acquire(&kmem[cpu_next].lock);
+    r = kmem[cpu_next].freelist;
+    if(r){
+      kmem[cpu_next].freelist = r->next;
+    }
+    release(&kmem[cpu_next].lock);
+    if(r){
+      break;
+    }
+  }
   return (void*)r;
 }
